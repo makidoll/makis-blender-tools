059802965f01faece27e55b9f28307bd8df6947c
Maki <mxmcube@gmail.com>
2018-09-02 14:50:14 +0100

    Added Split Plane

--- __init__.py
+++ __init__.py
@@ -65,10 +65,42 @@ class IcoPlane(bpy.types.Operator, bpy_extras.object_utils.AddObjectHelper):
 
 		return [verts, faces]
 
+class SplitPlane(bpy.types.Operator, bpy_extras.object_utils.AddObjectHelper):
+	bl_idname = "mesh.add_splitplane"
+	bl_label = "Add Split Plane"
+	bl_options = {"REGISTER", "UNDO"}
+
+	width = bpy.props.IntProperty(name="Width", default=16, min=1)
+	height = bpy.props.IntProperty(name="Height", default=16, min=1)
+	dist = bpy.props.FloatProperty(name="Distance", default=0.1)
+
+	def draw(self, context):
+		self.layout.prop(self, "width")
+		self.layout.prop(self, "height")
+		self.layout.prop(self, "dist")
+
+	def generate(self, w, h):
+		verts = []
+		faces = []
+		
+		for x in range(0, w):
+			for y in range(0, h):
+				cx = (x*self.dist)-(self.width*self.dist)/2
+				cy = (y*self.dist)-(self.height*self.dist)/2
+				verts.append(mathutils.Vector((cx,          cy,          0)))
+				verts.append(mathutils.Vector((cx+self.dist,cy,          0)))
+				verts.append(mathutils.Vector((cx,          cy+self.dist,0)))
+				verts.append(mathutils.Vector((cx+self.dist,cy+self.dist,0)))
+
+				i = (x+(y*self.width))*4
+				faces.append([i,i+1,i+3,i+2])
+
+		return [verts, faces]
+
 	def execute(self, context):
 		data = self.generate(self.width, self.height)
 
-		mesh = bpy.data.meshes.new(name="Ico Plane")
+		mesh = bpy.data.meshes.new(name="Split Plane")
 		mesh.from_pydata(data[0], [], data[1])
 		mesh.update(calc_edges=True)
 		bpy_extras.object_utils.object_data_add(context, mesh, operator=self)
@@ -93,13 +125,17 @@ class ToolPanel(bpy.types.Panel):
 		row = layout.row()
 		row.operator("mesh.add_icoplane")
 
+		row = layout.row()
+		row.operator("mesh.add_splitplane")
 
 def register():
 	bpy.utils.register_class(IcoPlane)
+	bpy.utils.register_class(SplitPlane)
 	bpy.utils.register_class(ToolPanel)
 
 def unregister():
 	bpy.utils.unregister_class(IcoPlane)
+	bpy.utils.unregister_class(SplitPlane)
 	bpy.utils.unregister_class(ToolPanel)
 
 if __name__ == "__main__":
